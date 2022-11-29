59c374a651af0831a3b6056762e8f299f18d5572
Vikrant Verma <87392159+crayzeerr@users.noreply.github.com>
2022-11-28 21:30:19 -0800

    added insertafter method

--- ListNode.java
+++ ListNode.java
@@ -6,13 +6,28 @@ public class ListNode {
         this.item = item;
         this.next = next;
     }
-
+ 
     public ListNode(Object item) {
         this(item, null);
     }
 
     public String toString() {
         return item.toString();
+    }
+
+    public void insertAfter(Object item) {
+        ListNode node = new ListNode(item, this.next);
+        //make new node object, assign item, set pointer to current nodes pointer
+        this.next = node;
+        //set current nodes pointer to new node
+
+        // node1 -> node 2
+
+        // node 1 ->    node 2
+        //          new node ^
+
+        //node 1 -> new node -> node 2
+
     }
 
 
